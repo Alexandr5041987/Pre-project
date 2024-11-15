0956e9c186450eb57139d85fc916b42182fe1e55
Alexandr5041987 <155677668+Alexandr5041987@users.noreply.github.com>
2024-11-15 20:46:35 +0300

    Update User.java

--- src/main/java/com/example/security_example/model/User.java
+++ src/main/java/com/example/security_example/model/User.java
@@ -104,4 +104,15 @@ public class User implements UserDetails {
     public void setEmail(String email) {
         this.email = email;
     }
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return email.equals(user.email);
+    }
+    @Override
+    public int hashCode() {
+        return email.hashCode();
+    }
 }
